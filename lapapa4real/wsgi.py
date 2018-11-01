e25d565a9810b6ac670470ef487175e1cac5a5a1
erichris <erichris@live.com.mx>
2018-11-01 17:26:35 -0600

    make it better3

--- lapapa4real/wsgi.py
+++ lapapa4real/wsgi.py
@@ -7,11 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lapapa4real.settings')
+#######
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'YOURAPP.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YOURAPP.settings")
+##########
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lapapa4real.settings')
 
 application = get_wsgi_application()
 
