faf0434b9e81bac21c497a78cd4a6ca7f96e9836
erichris <erichris@live.com.mx>
2018-11-01 17:32:11 -0600

    make it better3

--- lapapa4real/wsgi.py
+++ lapapa4real/wsgi.py
@@ -16,8 +16,8 @@ import os
 import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'YOURAPP.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YOURAPP.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lapapa4real.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lapapa4real.settings")
 ##########
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lapapa4real.settings')
